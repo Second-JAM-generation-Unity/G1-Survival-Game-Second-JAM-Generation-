7d20777fe96ef9fa318b40ba7adae237491c0ef4
BAYRON DAVID MORALES <bayron.david.morales@gmail.com>
2022-11-11 15:43:02 -0500

    Barra de vida

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public float vida = 100;
+    public Image barraDeVida;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        vida = Mathf.Clamp(vida, 0, 100);
+        barraDeVida.fillAmount = vida / 100;
+    }
+}
