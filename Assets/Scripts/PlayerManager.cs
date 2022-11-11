9db04a727f79a1fee6798fb5109f5102ecc5979e
BAYRON DAVID MORALES <bayron.david.morales@gmail.com>
2022-11-11 18:26:10 -0500

    Drag animation

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using StarterAssets;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,16 @@ using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
 {
+    public ThirdPersonController thirdPersonController;
+
     public float vida = 100;
     public Image barraDeVida;
 
+    public Animator animator;
+    public bool canPush = false;
+
+
+    public AudioSource drag;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +28,22 @@ public class PlayerManager : MonoBehaviour
     {
         vida = Mathf.Clamp(vida, 0, 100);
         barraDeVida.fillAmount = vida / 100;
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        animator.SetBool("Push", true);
+        thirdPersonController.MoveSpeed = 1.0f;
+        thirdPersonController.SprintSpeed = 1.0f;
+        drag.Play();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        animator.SetBool("Push", false);
+        thirdPersonController.MoveSpeed = 2.0f;
+        thirdPersonController.SprintSpeed = 5.335f;
+        drag.Pause();
     }
 }
