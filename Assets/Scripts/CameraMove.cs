de7b8af59b034bcac2fb154c8be40994c1f45580
lymancillal <lymancillal@gmail.com>
2022-11-11 13:23:13 -0500

    puente

--- Assets/Scripts/CameraMove.cs
+++ Assets/Scripts/CameraMove.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class CameraMove : MonoBehaviour
 {
-    public Transform player;
-    public float speed;
-    public Vector3 CameraPosition;
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
 
     // Update is called once per frame
-
-    private void Start()
-    {
-        //CameraPosition = new Vector3(player.position.x + 20, player.position.y + 2, player.position.z + 2);
-    }
     void FixedUpdate()
     {
-
-        Vector3 Dposition = player.position + CameraPosition;
-        Vector3 Sposition = Vector3.Lerp(transform.position, Dposition, speed * Time.deltaTime);
         //transform.LookAt(player.transform);
-        transform.position = Sposition;
+        transform.position = new Vector3(transform.position.x, player.transform.position.y + 4, player.transform.position.z);
     }
 }
